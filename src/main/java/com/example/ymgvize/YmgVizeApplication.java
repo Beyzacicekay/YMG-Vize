0f3bdc7ca6c76f9696af86a7b1a04f0c66acb869
Beyza <cicekaybeyza@gmail.com>
2024-05-07 13:47:50 +0300

    <a>

--- src/main/java/com/example/ymgvize/YmgVizeApplication.java
+++ src/main/java/com/example/ymgvize/YmgVizeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YmgVizeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YmgVizeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YmgVizeApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/ymgvize/YmgVizeApplicationTests.java
+++ src/test/java/com/example/ymgvize/YmgVizeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YmgVizeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
